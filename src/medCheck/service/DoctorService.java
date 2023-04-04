7bd2a0b2b9c33d803a5229349001a64d6403535a
Mirbek <mitokadze@gmail.com>
2023-04-04 17:48:58 +0600

    I wrote service interface

--- src/medCheck/service/DoctorService.java
+++ src/medCheck/service/DoctorService.java
@@ -1,4 +1,16 @@
 package medCheck.service;
 
+import medCheck.model.Doctor;
+
+import java.util.List;
+
 public interface DoctorService {
+    String addDoctorToHospital(Long id, Doctor doctor);
+    Doctor findDoctorById(Long id);
+    String updateDoctor(Long id, Doctor doctor);
+    void deleteDoctorById(Long id);
+    String assignDoctorToDepartment(Long departmentId, List<Long> doctorsId);
+    List<Doctor> getAllDoctorsByHospitalId(Long id);
+    List<Doctor> getAllDoctorsByDepartmentId(Long id);
+
 }
--- src/medCheck/service/PatientService.java
+++ src/medCheck/service/PatientService.java
@@ -1,4 +1,16 @@
 package medCheck.service;
 
+import medCheck.model.Patient;
+
+import java.util.List;
+import java.util.Map;
+
 public interface PatientService {
+    String addPatientToHospital(Long id, Patient patient);
+    String addPatientsToHospital(Long id,List<Patient> patients);
+    String updatePatientById(Long id, Patient patientsNewInfo);
+    void removePatientById(Long id);
+    Patient getPatientById(Long id);
+    Map<Integer, Patient> getPatientByAge();
+    List<Patient> sortPatientsByAge(String ascOrDesc);
 }
