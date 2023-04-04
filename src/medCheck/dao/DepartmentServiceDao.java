e3d18ba0ef356327c972386dc59a8bc17836f8a9
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-04-04 16:26:58 +0300

    added InterfaceDaoMethod

--- src/medCheck/dao/DepartmentServiceDao.java
+++ src/medCheck/dao/DepartmentServiceDao.java
@@ -2,7 +2,12 @@ package medCheck.dao;
 
 import medCheck.model.Department;
 
+import java.util.List;
+
 public interface DepartmentServiceDao {
     String addDepartmentToHospital(Long id, Department department);
-
+    List<Department> getAllDepartmentByHospital(Long id);
+    Department findDepartmentByName(String name);
+    void deleteDepartmentById(Long id);
+    String updateDepartmentById(Long id, Department department);
 }
