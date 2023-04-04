504b28ba8e3accb1eeb90546ab5eef17a686cea3
user <duyshenbekov. b@gmail.com>
2023-04-04 22:16:34 +0600

    added fields to Doctor class

--- src/medCheck/model/Doctor.java
+++ src/medCheck/model/Doctor.java
@@ -1,4 +1,13 @@
 package medCheck.model;
 
+import medCheck.enums.Gender;
+
 public class Doctor {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private int experienceYear;
+
 }
+
