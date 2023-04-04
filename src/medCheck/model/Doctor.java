6cd627d659e52040ffbe2768f72eabf29f2311c2
Bektur Duyshenbek uulu <duyshenbekov. b@gmail.com>
2023-04-04 23:07:04 +0600

    added constructor to doctor class

--- src/medCheck/model/Doctor.java
+++ src/medCheck/model/Doctor.java
@@ -1,4 +1,19 @@
 package medCheck.model;
 
+import medCheck.enums.Gender;
+
 public class Doctor {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private int experienceYear;
+
+    public Doctor(Long id, String firstName, String lastName, Gender gender, int experienceYear) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.experienceYear = experienceYear;
+    }
 }
