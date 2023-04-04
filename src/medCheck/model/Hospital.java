c54700a735f01d593331a3dd3f9206452c940b80
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-04-04 22:56:13 +0300

    added Hospital constructor

--- src/medCheck/model/Hospital.java
+++ src/medCheck/model/Hospital.java
@@ -1,4 +1,21 @@
 package medCheck.model;
 
+import java.util.List;
+
 public class Hospital {
+    private Long id;
+    private String hospitalName;
+    private String address;
+    private List<Department> departments;
+    private List<Doctor> doctors;
+    private List<Patient> patients;
+
+    public Hospital(Long id, String hospitalName, String address, List<Department> departments, List<Doctor> doctors, List<Patient> patients) {
+        this.id = id;
+        this.hospitalName = hospitalName;
+        this.address = address;
+        this.departments = departments;
+        this.doctors = doctors;
+        this.patients = patients;
+    }
 }
