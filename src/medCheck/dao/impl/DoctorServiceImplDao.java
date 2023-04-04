e0b747254130ed89d7a01247172d214710210c22
Bektur Duyshenbek uulu <duyshenbekov. b@gmail.com>
2023-04-05 00:33:05 +0600

    addDoctorToHospital to doctor class

--- src/medCheck/dao/impl/DoctorServiceImplDao.java
+++ src/medCheck/dao/impl/DoctorServiceImplDao.java
@@ -1,14 +1,36 @@
 package medCheck.dao.impl;
 
 import medCheck.dao.DoctorServiceDao;
+import medCheck.dao.HospitalServiceDao;
+import medCheck.database.Database;
 import medCheck.model.Doctor;
+import medCheck.model.Hospital;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DoctorServiceImplDao implements DoctorServiceDao {
+
+    private Map<Long,Doctor>doctors=new HashMap<>();
+    private List<Database>databases;
+    private List<HospitalServiceDao>hospitals;
+
+
+
     @Override
     public String addDoctorToHospital(Long id, Doctor doctor) {
-        return null;
+        HospitalServiceDao hospitalServiceDao = new HospitalServiceImplDao();
+        Hospital hospital = hospitalServiceDao.findHospitalById(id);
+        if(hospital!=null){
+            hospital.getDoctors().add(doctor);
+            doctors.put(doctor.getId(),doctor);
+            return "Doctor added to hospital successfully";
+        }else{
+            return "Hospital not found";
+        }
+
+
     }
 
     @Override
