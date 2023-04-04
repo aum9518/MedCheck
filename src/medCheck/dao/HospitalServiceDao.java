5c5ee29a6c3b70818fd41d0cfb5e834b3fa7186f
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-04-04 16:29:22 +0300

    added InterfaceDaoMethod

--- src/medCheck/dao/HospitalServiceDao.java
+++ src/medCheck/dao/HospitalServiceDao.java
@@ -1,4 +1,16 @@
 package medCheck.dao;
 
+import medCheck.model.Hospital;
+import medCheck.model.Patient;
+
+import java.util.List;
+import java.util.Map;
+
 public interface HospitalServiceDao {
+    String addHospital(Hospital hospital);
+    Hospital findHospitalById(Long id);
+    List<Hospital> getAllHospital();
+    List<Patient> getAllPatientFromHospital(Long id);
+    String deleteHospitalById(Long id);
+    Map<String, Hospital> getAllHospitalByAddress(String address);
 }
