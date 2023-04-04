b49fe8640b533b672a104f19281ead61a053aa93
Mirbek <mitokadze@gmail.com>
2023-04-04 18:06:48 +0600

    I wrote service impl methods

--- src/medCheck/service/serviceImpl/DepartmentServiceImpl.java
+++ src/medCheck/service/serviceImpl/DepartmentServiceImpl.java
@@ -1,4 +1,33 @@
 package medCheck.service.serviceImpl;
 
-public class DepartmentServiceImpl {
+import medCheck.model.Department;
+import medCheck.service.DepartmentService;
+
+import java.util.List;
+
+public class DepartmentServiceImpl implements DepartmentService {
+    @Override
+    public String addDepartmentToHospital(Long id, Department department) {
+        return null;
+    }
+
+    @Override
+    public List<Department> getAllDepartmentByHospital(Long id) {
+        return null;
+    }
+
+    @Override
+    public Department findDepartmentByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void deleteDepartmentById(Long id) {
+
+    }
+
+    @Override
+    public String updateDepartmentById(Long id, Department department) {
+        return null;
+    }
 }
--- src/medCheck/service/serviceImpl/HospitalServiceImpl.java
+++ src/medCheck/service/serviceImpl/HospitalServiceImpl.java
@@ -1,4 +1,40 @@
 package medCheck.service.serviceImpl;
 
-public class HospitalServiceImpl {
+import medCheck.model.Hospital;
+import medCheck.model.Patient;
+import medCheck.service.HospitalService;
+
+import java.util.List;
+import java.util.Map;
+
+public class HospitalServiceImpl implements HospitalService {
+    @Override
+    public String addHospital(Hospital hospital) {
+        return null;
+    }
+
+    @Override
+    public Hospital findHospitalById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Hospital> getAllHospital() {
+        return null;
+    }
+
+    @Override
+    public List<Patient> getAllPatientFromHospital(Long id) {
+        return null;
+    }
+
+    @Override
+    public String deleteHospitalById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Hospital> getAllHospitalByAddress(String address) {
+        return null;
+    }
 }
