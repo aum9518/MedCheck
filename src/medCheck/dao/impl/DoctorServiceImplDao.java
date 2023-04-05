4f53ce450df74e7d3f8b54741274c8564267f2b1
Bektur Duyshenbek uulu <duyshenbekov. b@gmail.com>
2023-04-05 12:39:21 +0600

    added function method addDoctorToHospital

--- src/medCheck/dao/impl/DoctorServiceImplDao.java
+++ src/medCheck/dao/impl/DoctorServiceImplDao.java
@@ -1,14 +1,32 @@
 package medCheck.dao.impl;
 
 import medCheck.dao.DoctorServiceDao;
+import medCheck.dao.HospitalServiceDao;
+import medCheck.database.Database;
 import medCheck.model.Doctor;
+import medCheck.model.Hospital;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 
 public class DoctorServiceImplDao implements DoctorServiceDao {
+
+    List<Database>databases;
+    Map<Long,Doctor>doctors;
+
     @Override
     public String addDoctorToHospital(Long id, Doctor doctor) {
-        return null;
+        HospitalServiceDao hospitalDao = new HospitalServiceImplDao();
+        Hospital hospital = hospitalDao.findHospitalById(id);
+        if (hospital != null) {
+            hospital.getDoctors().add(doctor);
+            doctors.put(doctor.getId(), doctor);
+            return "Doctor added to hospital successfully";
+        } else {
+            return "Hospital not found";
+        }
     }
 
     @Override
@@ -23,7 +41,7 @@ public class DoctorServiceImplDao implements DoctorServiceDao {
 
     @Override
     public void deleteDoctorById(Long id) {
-
+        doctors.remove(id);
     }
 
     @Override
@@ -38,6 +56,7 @@ public class DoctorServiceImplDao implements DoctorServiceDao {
 
     @Override
     public List<Doctor> getAllDoctorsByDepartmentId(Long id) {
-        return null;
+
+       return null;
     }
 }
