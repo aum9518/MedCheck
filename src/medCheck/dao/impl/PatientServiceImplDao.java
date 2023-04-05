9e45eec4a2d8d1055e809d812a4f2c3d39c24cca
manasabduganiuulu <abduganiuulumanas@gmail.com>
2023-04-05 13:31:53 +0600

    added method addPatientToHospital

--- src/medCheck/dao/impl/PatientServiceImplDao.java
+++ src/medCheck/dao/impl/PatientServiceImplDao.java
@@ -1,14 +1,37 @@
 package medCheck.dao.impl;
 
 import medCheck.dao.PatientServiceDao;
+import medCheck.database.Database;
+import medCheck.exception.MyException;
+import medCheck.model.Hospital;
 import medCheck.model.Patient;
 
 import java.util.List;
 import java.util.Map;
 
+
 public class PatientServiceImplDao implements PatientServiceDao {
+    private Database database;
     @Override
     public String addPatientToHospital(Long id, Patient patient) {
+        boolean isTrue = true;
+        for (Hospital d:database.getHospitals()) {
+            if (d.getId()==id){
+                isTrue = true;
+                d.getPatients().add(patient);
+            }else {
+                isTrue = false;
+            }
+        }
+        try{
+            if (isTrue){
+                return "Patient successfully added patient: "+patient;
+            }else {
+                throw new MyException("Not found hospital");
+            }
+        } catch (MyException e) {
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
