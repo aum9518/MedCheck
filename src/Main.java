be34dbadebad92761734d573b5ce3bca6b7de09a
Bektur Duyshenbek uulu <duyshenbekov. b@gmail.com>
2023-04-05 13:46:51 +0600

    Added function methods

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import medCheck.enums.Gender;
+import medCheck.model.Doctor;
+import medCheck.service.serviceImpl.DoctorServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Hello ");
+
+
 
 
     }
